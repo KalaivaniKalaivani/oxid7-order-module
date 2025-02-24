61024564c357fdfad45588cf58454791ec2a6fb2
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-02-24 15:08:54 +0530

    Create metadata.php

---
+++ metadata.php
@@ -0,0 +1,23 @@
+<?php
+
+/***
+ * metadata version
+ */
+ $sMetadataVersion = '2.1';
+ 
+ /***
+  * Module information
+  */
+$aModule = [
+		'id' => 'novalnetOrder',
+		'title' => 'Novalnet Orders'
+		'description' => [
+			'en' => 'Shows the only Novalnet Payment Orders in Shop Admin page',
+			'de' => 'Shows the only Novalnet Payment Orders in Shop Admin page'
+		],
+		'thumbnail' => 'img/novalnet_logo.png',
+		'version' => '1.0.0',
+		'author' => 'Novalnet AG',
+		'url'         => 'https://www.novalnet.de',
+        'email'       => 'technic@novalnet.de'
+]; 
