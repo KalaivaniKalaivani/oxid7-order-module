7dd2762fb3a6be28a04b9597fbb0835f82722087
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-02-25 15:45:53 +0530

    Create novalnet_lang.php

---
+++ views/admin_twig/de/novalnet_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$sLangName = 'English';
+
+$aLang = [
+    'charset'               => 'UTF-8',
+    'TAB_NOVALNET_ORDERS'   => 'Novalnet ',
+    'mxnovalnetorders'      => 'Novalnet Orders',
+    'mxdisplaynovalnetorders'   => 'Novalnet',
+   
+];
