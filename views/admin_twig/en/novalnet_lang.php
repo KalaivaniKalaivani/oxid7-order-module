d6e05727f080b9f93a04ce693fe8c6a08e3f05c6
KalaivaniKalaivani <144912811+KalaivaniKalaivani@users.noreply.github.com>
2025-02-25 15:46:14 +0530

    Update novalnet_lang.php

--- views/admin_twig/en/novalnet_lang.php
+++ views/admin_twig/en/novalnet_lang.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Novalnet payment module
- *
- * This file is used for the admin English language.
- *
- * @author    Novalnet AG
- * @copyright Copyright by Novalnet
- * @link      https://www.novalnet.de
- * @license   https://www.novalnet.de/payment-plugins/kostenlos/lizenz
- *
- * Script: novalnet_lang.php
- */
-
 $sLangName = 'English';
 
 $aLang = [
